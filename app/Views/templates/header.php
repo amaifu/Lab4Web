74086eb57da2f13ff1a78a03dca627febc8c8359
amaifu <ahmadsaepudin032@gmail.com>
2025-04-22 09:51:39 +0700

    commit for p3

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -17,4 +17,12 @@
             <a href="<?= base_url('/contact');?>">Kontak</a>
         </nav>
         <section id="wrapper">
+            <section id="hero">
+                <h1>Hello World!</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lorem
+                elit, iaculis innisl volutpat, malesuada tincidunt arcu. Proin in leo fringilla,
+                vestibulum mi porta, faucibus felis. Integer pharetra est nunc, nec pretium nunc
+                pretium ac.</p>
+                <a href="home.html" class="btn btn-large">Learn more &raquo;</a>
+            </section>
             <section id="main">
